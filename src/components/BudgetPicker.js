566ccbb5141569668dbc80a187638b322f9461b8
Juan Martín García <hello@juangarcia.com.ar>
2019-04-26 16:35:22 -0300

    Add project starter files

---
+++ src/components/BudgetPicker.js
@@ -0,0 +1,15 @@
+import React from "react";
+
+class BudgetPicker extends React.Component {
+  render() {
+    return (
+      <form action="">
+        <h2>Please Enter a Name</h2>
+        <input type="text" required placeholder="Enter the name for the tool" />
+        <button type="submit">Go to the tool</button>
+      </form>
+    );
+  }
+}
+
+export default BudgetPicker;
---
+++ src/index.js
@@ -0,0 +1,5 @@
+import React from "react";
+import { render } from "react-dom";
+import BudgetPicker from "./components/BudgetPicker";
+
+render(<BudgetPicker />, document.querySelector("#main"));
