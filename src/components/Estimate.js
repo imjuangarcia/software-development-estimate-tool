842d8f4da14c3770cd089c6d58c5c64fbc025303
Juan Martín García <hello@juangarcia.com.ar>
2019-04-30 20:15:03 -0300

    Fix expected hour calculation on Estimate component

--- src/components/Estimate.js
+++ src/components/Estimate.js
@@ -7,33 +7,39 @@ class Estimate extends React.Component {
     estimate: PropTypes.object,
     removeFromEstimate: PropTypes.func
   };
+
   renderEstimate = key => {
     const task = this.props.tasks[key];
     const count = this.props.estimate[key];
-    return (
-      <li key={key}>
-        {count} unidad {task.taskName}
-        <button onClick={() => this.props.removeFromEstimate(key)}>
-          &times;
-        </button>
-      </li>
-    );
+    if (task) {
+      return (
+        <li key={key}>
+          {count} unidad {task.taskName}
+          <button onClick={() => this.props.removeFromEstimate(key)}>
+            &times;
+          </button>
+        </li>
+      );
+    }
   };
   render() {
     const estimateIds = Object.keys(this.props.estimate);
-    const total = estimateIds.reduce((prevTotal, key) => {
-      const task = this.props.tasks[key];
-      const count = this.props.estimate[key];
-
-      return prevTotal + count * task.expectedHours;
-    }, 0);
     return (
       <div className="order-wrap">
         <h2>Estimate</h2>
         <ul className="order">{estimateIds.map(this.renderEstimate)}</ul>
         <div className="total">
           Total
-          <strong>{total}</strong> horas
+          <strong>
+            {estimateIds.reduce((prevTotal, key) => {
+              const task = this.props.tasks[key];
+              if (task != undefined) {
+                const count = this.props.estimate[key];
+                return prevTotal + count * task.expectedHours;
+              }
+            }, 0)}
+          </strong>{" "}
+          horas
         </div>
       </div>
     );
