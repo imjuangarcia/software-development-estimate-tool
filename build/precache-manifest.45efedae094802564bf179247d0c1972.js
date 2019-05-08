bddb0fc09fafb6fdbaa7a0a16582cc4150609788
Juan Martín García <hello@juangarcia.com.ar>
2019-05-08 13:50:01 -0300

    Add production build

---
+++ build/precache-manifest.45efedae094802564bf179247d0c1972.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "322ab3d1346f4234338bab94b6f834e1",
+    "url": "/index.html"
+  },
+  {
+    "revision": "ae416535bf5d77e9ddd6",
+    "url": "/static/css/main.e4ba0cc8.chunk.css"
+  },
+  {
+    "revision": "c037793df6354e5e28a4",
+    "url": "/static/js/2.a5fe5e18.chunk.js"
+  },
+  {
+    "revision": "ae416535bf5d77e9ddd6",
+    "url": "/static/js/main.1a6652fc.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
