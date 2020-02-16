42a994687a9eee722f263532fd76e177494079a5
Juan Martín García <hello@juangarcia.com.ar>
2020-02-16 12:35:42 -0300

    Added new routes for auth and create estimate

--- src/components/Router.js
+++ src/components/Router.js
@@ -1,13 +1,15 @@
 import React from 'react';
 import { BrowserRouter, Route, Switch } from 'react-router-dom';
-import EstimateForm from './EstimateForm';
+import EstimateForm from './components/EstimateForm';
+import Auth from './Auth';
 import App from './App';
 import NotFound from './NotFound';
 
 const Router = () => (
   <BrowserRouter>
     <Switch>
-      <Route exact path="/" component={EstimateForm} />
+      <Route exact path="/" component={Auth} />
+      <Route exact path="/create/" component={EstimateForm} />
       <Route path="/estimate/:estimateId" component={App} />
       <Route component={NotFound} />
     </Switch>
