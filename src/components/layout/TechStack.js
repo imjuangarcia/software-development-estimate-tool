80a4b968515b35cc6c2419df8c725072695b45f7
Juan Martín García <hello@juangarcia.com.ar>
2020-02-23 18:58:00 -0300

    Refactor Tech Stack to use Hooks

--- src/components/layout/TechStack.js
+++ src/components/layout/TechStack.js
@@ -1,39 +1,19 @@
-import React from "react";
+import React, { useRef } from "react";
 import AddTechStackForm from "../components/AddTechStackForm";
 
-class TechStack extends React.Component {
+const TechStack = (props) => {
   // Refs
-  techStackFormRef = React.createRef();
+  const techStackFormRef = useRef();
 
-  // Lifecycle methods
-  componentDidUpdate() {
-    if (this.techStackFormRef.current !== null) {
-      if (this.props.auth === undefined) {
-        // if they're not logged in
-        this.techStackFormRef.current.classList.add("hidden");
-      }
-      // if they're logged in, but they're now owners
-      else if (this.props.auth.owner !== this.props.auth.uid) {
-        this.techStackFormRef.current.classList.add("hidden");
-      }
-      // if they're owners
-      else {
-        this.techStackFormRef.current.classList.remove("hidden");
-      }
-    }
-  }
-
-  render() {
-    return (
-      <section className="tech edit" ref={this.techStackFormRef}>
-        <div className="title">
-          <h2>Technologies to use</h2>
-        </div>
-        <h3>Add a New Tech</h3>
-        <AddTechStackForm addTechStack={this.props.addTechStack} />
-      </section>
-    );
-  }
+  return (
+    <section className={props.auth && props.auth.owner === props.auth.uid ? 'tech edit' : 'tech edit hidden'} ref={techStackFormRef}>
+      <div className="title">
+        <h2>Technologies to use</h2>
+      </div>
+      <h3>Add a New Tech</h3>
+      <AddTechStackForm addTechStack={props.addTechStack} />
+    </section>
+  );
 }
 
 export default TechStack;
