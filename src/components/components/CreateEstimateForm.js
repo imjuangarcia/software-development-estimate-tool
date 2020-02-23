616998f3e729602fd73feb3971a164d3ed139661
Juan Martín García <hello@juangarcia.com.ar>
2020-02-23 09:55:18 -0300

    Refactor `CreateEstimateForm` to use Hooks

--- src/components/components/CreateEstimateForm.js
+++ src/components/components/CreateEstimateForm.js
@@ -1,4 +1,4 @@
-import React, { Fragment } from "react";
+import React, { Fragment, useRef, useEffect } from "react";
 
 import { slugify } from "../../utilities/helpers";
 import base from "../../firebase";
@@ -6,133 +6,138 @@ import base from "../../firebase";
 import Header from '../layout/Header'
 import Footer from '../layout/Footer'
 
-class EstimateCreator extends React.Component {
-  projectNameRef = React.createRef();
-  clientNameRef = React.createRef();
-  dateRef = React.createRef();
-  versionNumberRef = React.createRef();
-  estimateNumberRef = React.createRef();
+const EstimateCreator = (props) => {
+  // Refs
+  const projectNameRef = useRef();
+  const clientNameRef = useRef();
+  const dateRef = useRef();
+  const versionNumberRef = useRef();
+  const estimateNumberRef = useRef();
 
-  state = {
-    auth: {},
-    estimateNumber: 1,
-  };
-
-  componentDidMount() {
-    // We check if the user is authenticated
-    const { owner, uid } = this.props.history.location.auth || '';
-    // If it is, we set that data to state
-    if(owner && uid) {
-      this.setState({
-        auth: {
-          owner: this.props.history.location.auth.owner,
-          uid: this.props.history.location.auth.uid,
-        }
-      });
-    } 
-    // If not, we redirect them to the log in page
-    else {
-      this.props.history.push('/');
-    }
-
+  useEffect(() => {
     // Fetch the existing projects on the db
     base.fetch('/', {
       context: this,
       asArray: true,
     }).then(data => {
       // Set the count of projects + 1 as the value for the input
-      if(this.estimateNumberRef.current === null) {
+      if(estimateNumberRef.current === null) {
         return;
       } else {
-        this.estimateNumberRef.current.value = data.length + 1;
+        estimateNumberRef.current.value = data.length + 1;
       }
     })
-  }
+  }, []);
 
-  goToEstimate = async event => {
+  // state = {
+  //   auth: {},
+  //   estimateNumber: 1,
+  // };
+
+  // componentDidMount() {
+    // We check if the user is authenticated
+    // const { owner, uid } = this.props.history.location.auth || '';
+    // // If it is, we set that data to state
+    // if(owner && uid) {
+    //   this.setState({
+    //     auth: {
+    //       owner: this.props.history.location.auth.owner,
+    //       uid: this.props.history.location.auth.uid,
+    //     }
+    //   });
+    // } 
+    // // If not, we redirect them to the log in page
+    // else {
+    //   this.props.history.push('/');
+    // }
+  // }
+
+  const goToEstimate = async event => {
     event.preventDefault();
+
     // Get the text from the input and create the slug
-    const estimate = slugify(this.projectNameRef.current.value);
+    const estimate = slugify(projectNameRef.current.value);
 
     // Send this data to firebase
     await base.post(`${estimate}/`, {
       data: {
-        owner: this.props.history.location.auth.owner,
+        // owner: props.history.location.auth.owner,
         client: {
-          projectName: this.projectNameRef.current.value,
-          clientName: this.clientNameRef.current.value,
-          date: this.dateRef.current.value,
-          versionNumber: parseFloat(this.versionNumberRef.current.value),
-          estimateNumber: parseFloat(this.estimateNumberRef.current.value)
+          projectName: projectNameRef.current.value,
+          clientName: clientNameRef.current.value,
+          date: dateRef.current.value,
+          versionNumber: parseFloat(versionNumberRef.current.value),
+          estimateNumber: parseFloat(estimateNumberRef.current.value)
         }
       }
     });
 
-    const auth = this.state.auth;
+    // const auth = this.state.auth;
 
     // Redirect to tasks list
-    this.props.history.push({ pathname: `/estimate/${estimate}`, auth});
+    // this.props.history.push({ pathname: `/estimate/${estimate}`, auth});
+    props.history.push({ pathname: `/estimate/${estimate}`});
   };
-  render() {
-    return (
-      <Fragment>
-        <Header history={this.props.history} />
-        <section className="create-project">
-          <form action="" onSubmit={this.goToEstimate}>
-            <h2>Please Enter the details of the project to estimate</h2>
-            <fieldset>
-              <label htmlFor="projectName">Project Name</label>
-              <input
-                type="text"
-                ref={this.projectNameRef}
-                required
-                placeholder="Enter the Project Name Here"
-                name="projectName"
-              />
-            </fieldset>
-            <fieldset>
-              <label htmlFor="clientName">Client Name</label>
-              <input
-                type="text"
-                ref={this.clientNameRef}
-                required
-                placeholder="Enter the Client Name Here"
-                name="clientName"
-              />
-            </fieldset>
-            <fieldset>
-              <label htmlFor="date">Estimate Date</label>
-              <input type="date" ref={this.dateRef} required name="date" />
-            </fieldset>
-            <fieldset>
-              <label htmlFor="versionNumber">Estimate Version Number</label>
-              <input
-                type="number"
-                ref={this.versionNumberRef}
-                required
-                placeholder="Enter Version Number Here"
-                name="versionNumber"
-                defaultValue={1.0}
-                step="0.1"
-              />
-            </fieldset>
-            <fieldset>
-              <label htmlFor="estimateNumber">Estimate Number</label>
-              <input
-                type="number"
-                ref={this.estimateNumberRef}
-                required
-                name="estimateNumber"
-                placeholder="Enter Estimate Number Here"
-              />
-            </fieldset>
-            <button type="submit">Go to the estimate</button>
-          </form>
-        </section>
-        <Footer />
-      </Fragment>
-    );
-  }
+
+  return (
+    <Fragment>
+      <Header history={props.history} />
+      <section className="create-project">
+        <form action="" onSubmit={goToEstimate}>
+          <h2>Please Enter the details of the project to estimate</h2>
+          <fieldset>
+            <label htmlFor="projectName">Project Name</label>
+            <input
+              type="text"
+              ref={projectNameRef}
+              required
+              placeholder="Enter the Project Name Here"
+              name="projectName"
+            />
+          </fieldset>
+          <fieldset>
+            <label htmlFor="clientName">Client Name</label>
+            <input
+              type="text"
+              ref={clientNameRef}
+              required
+              placeholder="Enter the Client Name Here"
+              name="clientName"
+            />
+          </fieldset>
+          <fieldset>
+            <label htmlFor="date">Estimate Date</label>
+            <input type="date" ref={dateRef} required name="date" />
+          </fieldset>
+          <fieldset>
+            <label htmlFor="versionNumber">Estimate Version Number</label>
+            <input
+              type="number"
+              ref={versionNumberRef}
+              required
+              placeholder="Enter Version Number Here"
+              name="versionNumber"
+              defaultValue={1.0}
+              step="0.1"
+            />
+          </fieldset>
+          <fieldset>
+            <label htmlFor="estimateNumber">Estimate Number</label>
+            <input
+              type="number"
+              ref={estimateNumberRef}
+              required
+              name="estimateNumber"
+              defaultValue={1}
+              placeholder="Enter Estimate Number Here"
+            />
+          </fieldset>
+          <button type="submit">Go to the estimate</button>
+        </form>
+      </section>
+      <Footer />
+    </Fragment>
+  );
 }
 
 export default EstimateCreator;
