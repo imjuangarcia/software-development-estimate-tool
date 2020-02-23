0946f028d472ba3dd6d3619bd87ec5ff5c2fad1e
Juan Martín García <hello@juangarcia.com.ar>
2020-02-23 18:50:47 -0300

    Refactor Tasks to use Hooks

--- src/components/layout/Tasks.js
+++ src/components/layout/Tasks.js
@@ -2,32 +2,29 @@ import React from "react";
 import AddTaskForm from "../components/AddTaskForm";
 import EditTaskForm from "../components/EditTaskForm";
 
-class Tasks extends React.Component {
-  render() {
-    // They must be the owner
-    return (
-      <section className={this.props.auth === undefined || this.props.auth.owner !== this.props.auth.uid ? "hidden" : "tasks edit" }>
-        <div className="title">
-          <h2>Tasks</h2>
-        </div>
-        {Object.keys(this.props.tasks).map(key => (
-          <EditTaskForm
-            key={key}
-            index={key}
-            task={this.props.tasks[key]}
-            updateTask={this.props.updateTask}
-            deleteTask={this.props.deleteTask}
-            addToEstimate={this.props.addToEstimate}
-          />
-        ))}
-        <h3>Add a New Task</h3>
-        <AddTaskForm addTask={this.props.addTask} />
-        <button className="sample" onClick={this.props.loadSampleTasks}>
-          Load Sample Tasks
-        </button>
-      </section>
-    );
-  }
+const Tasks = (props) => {
+  return (
+    <section className={props.auth === undefined || props.auth.owner !== props.auth.uid ? "hidden" : "tasks edit" }>
+      <div className="title">
+        <h2>Tasks</h2>
+      </div>
+      {Object.keys(props.tasks).map(key => (
+        <EditTaskForm
+          key={key}
+          index={key}
+          task={props.tasks[key]}
+          updateTask={props.updateTask}
+          deleteTask={props.deleteTask}
+          addToEstimate={props.addToEstimate}
+        />
+      ))}
+      <h3>Add a New Task</h3>
+      <AddTaskForm addTask={props.addTask} />
+      <button className="sample" onClick={props.loadSampleTasks}>
+        Load Sample Tasks
+      </button>
+    </section>
+  );
 }
 
 export default Tasks;
