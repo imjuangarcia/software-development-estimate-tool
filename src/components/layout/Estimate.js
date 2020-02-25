db4dca2aa9e4b8f85b4605221a198d556e329857
Juan Martín García <hello@juangarcia.com.ar>
2020-02-25 19:11:57 -0300

    Refactor Estimate to be stateless functional

--- src/components/layout/Estimate.js
+++ src/components/layout/Estimate.js
@@ -5,104 +5,67 @@ import Costs from "./Costs";
 import Resources from "./Resources";
 import Deadlines from "./Deadlines";
 import TaskEstimation from "../components/TaskEstimation";
-import base from "../../firebase";
 
-class Estimate extends React.Component {
-
-  // State
-  state = {
-    time: {},
-    resources: {},
-    cost: {}
-  };
-
-  // Lifecycle methods
-  componentDidMount() {
-    this.ref = base.syncState(`${this.props.estimateId}/time`, {
-      context: this,
-      state: "time"
-    });
-    this.ref = base.syncState(`${this.props.estimateId}/cost`, {
-      context: this,
-      state: "cost"
-    });
-  }
-
-  // componentDidUpdate() {
-  //   if (
-  //     this.totalTimeRef.current.value !== "" &&
-  //     this.adminTimeRef.current.value !== undefined &&
-  //     this.adminTimeRef.current.value !== ""
-  //   ) {
-  //     this.calculateTotalHours();
-  //   }
-  // }
-
-  componentWillUnmount() {
-    base.removeBinding(this.ref);
-  }
-
-  render() {
-    return (
-      <React.Fragment>
-        <EstimateProvider>
-          <EstimateContext.Consumer>
-            {context => (
-              <section className="estimate">
-                <h2>Work Plan Summary</h2>
-                <p>
-                  The following is a summary of the Time, Costs, Resources, and
-                  Deadlines estimates, all of which will be covered in further detail
-                  below.
-                </p>
-                <Time
-                  estimate={this.props.estimate}
-                  tasks={this.props.tasks}
-                  time={context.time}
-                  adminTimeRef={context.adminTimeRef}
-                  totalTimeRef={context.totalTimeRef}
-                  calculateTotalHours={context.calculateTotalHours}
-                  owner={this.props.owner}
-                  user={this.props.user}
-                />
-                <Costs
-                  hourlyValueRef={context.hourlyValueRef}
-                  owner={this.props.owner}
-                  user={this.props.user}
-                  cost={context.cost}
-                  calculateTotalPrice={context.calculateTotalPrice}
-                />
-                <Resources
-                  resources={this.props.resources}
-                  addResource={this.props.addResource}
-                  updateResource={this.props.updateResource}
-                  deleteResource={this.props.deleteResource}
-                  user={this.props.user}
-                  owner={this.props.owner}
-                />
-                <Deadlines
-                  time={context.time ? context.time : ''}
-                  resources={this.props.resources}
-                  minTimeRef={context.minTimeRef}
-                  avgTimeRef={context.avgTimeRef}
-                  maxTimeRef={context.maxTimeRef}
-                  expectedTimeRef={context.expectedTimeRef}
-                  calculateDeadlines={context.calculateDeadlines}
-                />
-                <TaskEstimation 
-                  estimate={this.props.estimate}
-                  tasks={this.props.tasks}
-                  owner={this.props.owner}
-                  user={this.props.user}
-                  removeFromEstimate={this.props.removeFromEstimate}
-                />
-              </section>
-            )}
-          </EstimateContext.Consumer>
-        </EstimateProvider>
-      </React.Fragment>
-    );
-  }
+const Estimate = (props) => {
+  return (
+    <React.Fragment>
+      <EstimateProvider estimateId={props.estimateId}>
+        <EstimateContext.Consumer>
+          {context => (
+            <section className="estimate">
+              <h2>Work Plan Summary</h2>
+              <p>
+                The following is a summary of the Time, Costs, Resources, and
+                Deadlines estimates, all of which will be covered in further detail
+                below.
+              </p>
+              <Time
+                estimate={props.estimate}
+                tasks={props.tasks}
+                time={context.time}
+                adminTimeRef={context.adminTimeRef}
+                totalTimeRef={context.totalTimeRef}
+                calculateTotalHours={context.calculateTotalHours}
+                owner={props.owner}
+                user={props.user}
+              />
+              <Costs
+                hourlyValueRef={context.hourlyValueRef}
+                owner={props.owner}
+                user={props.user}
+                cost={context.cost}
+                calculateTotalPrice={context.calculateTotalPrice}
+              />
+              <Resources
+                resources={props.resources}
+                addResource={props.addResource}
+                updateResource={props.updateResource}
+                deleteResource={props.deleteResource}
+                user={props.user}
+                owner={props.owner}
+              />
+              <Deadlines
+                time={context.time ? context.time : ''}
+                resources={props.resources}
+                minTimeRef={context.minTimeRef}
+                avgTimeRef={context.avgTimeRef}
+                maxTimeRef={context.maxTimeRef}
+                expectedTimeRef={context.expectedTimeRef}
+                calculateDeadlines={context.calculateDeadlines}
+              />
+              <TaskEstimation 
+                estimate={props.estimate}
+                tasks={props.tasks}
+                owner={props.owner}
+                user={props.user}
+                removeFromEstimate={props.removeFromEstimate}
+              />
+            </section>
+          )}
+        </EstimateContext.Consumer>
+      </EstimateProvider>
+    </React.Fragment>
+  );
 }
 
 export default Estimate;
